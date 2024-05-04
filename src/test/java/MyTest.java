e8aec255e2e189f9fe49cd1496d1a96510e2a2a1
Eloise2011 <joshuahbrooks@163.com>
2024-05-04 21:40:06 +0800

    5th commit - added notes for 4th commit

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -22,12 +22,18 @@ public class MyTest {
 
     @Test
     public void test2() {
+        //1. 创建模版引擎
         TemplateEngine te = new TemplateEngine();
+        //2. 读取磁盘钟的模版文件
         ClassLoaderTemplateResolver ctr = new ClassLoaderTemplateResolver();
+        //3.设定引擎使用的解析器
         te.setTemplateResolver(ctr);
+        //4. 制定数据
         Context context = new Context();
         context.setVariable("name", "Joshua Brooks");
+        //5.处理模版
         String out = te.process("main.html", context);
+        //6. 输出结果
         System.out.println("out = " + out);
     }
 
