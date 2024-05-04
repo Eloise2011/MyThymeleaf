5dc40c0cbbca8ae8286f02ad172995134ba33772
Eloise2011 <joshuahbrooks@163.com>
2024-05-04 21:37:25 +0800

    4th commit - second test - load template from existing html file

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 /**
  * @author Joshua.H.Brooks
@@ -10,12 +11,23 @@ import org.thymeleaf.context.Context;
 public class MyTest {
 
     @Test
-    public void test1(){
+    public void test1() {
         TemplateEngine te = new TemplateEngine();
         Context context = new Context();
         String input = "<input type='text' th:value=${course}/>";
-        context.setVariable("course","Learning Thymeleaf stupid way...");
+        context.setVariable("course", "Learning Thymeleaf stupid way...");
         String out = te.process(input, context);
+        System.out.println("out = " + out);
+    }
+
+    @Test
+    public void test2() {
+        TemplateEngine te = new TemplateEngine();
+        ClassLoaderTemplateResolver ctr = new ClassLoaderTemplateResolver();
+        te.setTemplateResolver(ctr);
+        Context context = new Context();
+        context.setVariable("name", "Joshua Brooks");
+        String out = te.process("main.html", context);
         System.out.println("out = " + out);
     }
 
